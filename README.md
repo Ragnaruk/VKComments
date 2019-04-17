8cef8c2c85734095f0a0a428706817effc14e1cc
Ragnaruk <ima1365@me.com>
2019-04-17 21:20:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+###Links
+* https://vk.com/dev/wall.getComments
+* https://vk.com/editapp?id=6947304&section=options
---
+++ options.py
@@ -0,0 +1,16 @@
+auth_type = 'token'
+access_token = '***REMOVED***'
+
+# auth_type = 'userpass'
+login = ''
+password = ''
+
+api_version = '5.95'
+owner_id = '-28122932'
+post_id = '220566'
+need_likes = '1'
+count = '100'
+sort = 'asc'
+thread_items_count = '0'
+
+return_fields = ['from_id', 'date', 'text']
