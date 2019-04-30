c5881fe474539a3bdf0a6483d6d7d65819e0bb75
Ragnaruk <ima1365@me.com>
2019-04-30 13:10:30 +0300

    Changed api requests to work with videos Added authorization via username/password Small fix to regex in parser Added TODO.txt file

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from vkcomments import VKComments
 # url = 'https://vk.com/search?c%5Bper_page%5D=80&c%5Bq%5D=прямая&c%5Bsection%5D=video&c%5Bsort%5D=2&z=video-2784806_456239168'
 # url = 'https://vk.com/feed?w=wall-28122932_228936'
 
+url = 'https://vk.com/video1009205_456239050'
+
 comments = VKComments(url)
+
 data = comments.get_comments()
+
 comments.print_csv(data)
