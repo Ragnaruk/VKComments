2ccb9124815e77f991b38a3831da2f1d32471df1
Ragnaruk <ima1365@me.com>
2019-05-28 21:37:49 +0300

    Changed previous tests and added one more

--- test/test_vkcomments.py
+++ test/test_vkcomments.py
@@ -3,24 +3,31 @@ from main.vkcomments import VKComments
 
 
 class TestVKComments(unittest.TestCase):
-    def test_get_ids_from_url__correct(self):
+    def test_get_ids_from_url__correct_person(self):
         obj = VKComments()
 
         self.assertEqual(
-            ('1009205', '456239050'), obj.get_ids_from_url("https://vk.com/video1009205_456239050")
+            ('123123', '456456'), obj.get_ids_from_url("https://vk.com/video123123_456456")
+        )
+
+    def test_get_ids_from_url__correct_group(self):
+        obj = VKComments()
+
+        self.assertEqual(
+            ('-123123', '456456'), obj.get_ids_from_url("https://vk.com/video-123123_456456")
         )
 
     def test_get_ids_from_url__too_many_numbers(self):
         obj = VKComments()
 
         with self.assertRaises(ValueError):
-            obj.get_ids_from_url("https://vk.com/video1009205_456239050_12345")
+            obj.get_ids_from_url("https://vk.com/video123123_456456_123456")
 
     def test_get_ids_from_url__not_enough_numbers(self):
         obj = VKComments()
 
         with self.assertRaises(ValueError):
-            obj.get_ids_from_url("https://vk.com/video1009205")
+            obj.get_ids_from_url("https://vk.com/video123123")
 
 
 if __name__ == '__main__':
